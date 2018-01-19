da6512060386411a6432e0ea9b632ab5156ec3dd
NeguliSimanis <simanism@inbox.lv>
2018-01-19 09:33:01 +0200

    Commit #6
    visul improvements to start page

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -6,14 +6,16 @@
             <h1 class="center">Available save files</h1>
             
             <!-- Table with uploaded files -->
-            <ul>
-                @foreach ($files as $file)
-                    <li>{{ $file }}</li>
-                @endforeach
-            </ul>
-            <?php echo app()->getLocale();?>
+            <div class="startpanel center">
+                <table id="file-table">
+                    @foreach ($files as $file)
+                        <tr>{{ $file }}</tr>
+                    @endforeach
+                </table>
+            </div>
+            
             <div class="center"> 
-                <a href="{{ url('upload') }}" class="btn btn-xs btn-info">Upload</a>
+                <a href="{{ url('upload') }}" class="btn btn-info">Upload</a>
             </div>
        </div>
     </div>
--- resources/views/startpage.blade.php
+++ resources/views/startpage.blade.php
@@ -4,11 +4,22 @@
        <div class="panel-body">
         <body>
             <h1 class="center">{{__('webContent.welcome') }}</h1>
-            <button>Register</button>
-            <button>Login</button>
-             <a class="btn btn-danger" href="{{ route('glogin') }}"> Login with Google</a>
-            <a href="{{ url('main') }}" class="btn btn-xs btn-info pull-right">Continue without login</a>
-            <?php echo app()->getLocale();?>
+            
+            <div class="startpanel center">
+               <!--  <div class="startbutton center"> -->
+                    <a class="btn btn-danger startbutton" href="{{ route('glogin') }}"> Register</a>
+               <!--  </div>
+                <div class="startbutton center">-->
+                    <a class="btn btn-danger startbutton" href="{{ route('glogin') }}"> Login</a>
+               <!--  </div>
+                <div class="startbutton center">-->
+                    <a class="btn btn-danger startbutton" href="{{ route('glogin') }}"> Login with Google</a>
+               <!--  </div>
+                <div class="startbutton center ">-->
+                    <a href="{{ url('main') }}" class="btn btn-info startbutton">Continue without login</a>
+               <!-- </div>-->
+            </div>
+            
             <meta name="google-signin-client_id" content="769663979583-fb17j53uhbrt5k53pq2mttngm8418tc6.apps.googleusercontent.com">
         </body>
     </div>
